febee4b8f211f7ddc747be4d5cf3c8d93c0c57d8
RahulShukla387 <KnowMeTechnical@gmail.com>
2024-10-05 12:59:42 +0530

    adding files

---
+++ chapter2Remainder.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (){
+    // int a=5, b=10;
+    // printf("the value of a is %d and value of b is %d " ,a,b);
+    
+    // Modulous is used to find the remainder 
+    int a = 2, b = 9;
+    printf("The remainder is %d when b is divided by a",b%a);
+    //a^b is not used for exponentation in c language
+    // arithmetic operator between int and int is always a int 
+    return 0;
+} 
---
+++ practiceSet.c
@@ -0,0 +1,14 @@
+//Practice question 1
+#include<stdio.h>
+int main(){
+// printf("enter the number");
+// int a;
+// scanf ("%d",&a);
+
+//    printf("%d",a%97); /* code */
+//Practice problem 2
+int y = 3*2/3-3+1;
+printf("the value of given equation is %d",y);
+
+    return 0;
+}
